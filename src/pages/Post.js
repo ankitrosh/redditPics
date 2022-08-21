e9908d81f0ae6af1f33d0477f410499c42a4741b
ankitrosh <roshantalluri@gmail.com>
2022-08-21 22:37:22 +0530

    added awards list

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -6,6 +6,33 @@ export default function Post(){
     const {postId} = useParams()
     const thisPost = allPhotos.find(pic => pic.data.id === postId)
     const history = useHistory();
+    const numAwardTypes = thisPost.data.all_awardings.length
+    let awardsClass
+    if(numAwardTypes < 3){
+        awardsClass = "smallAwards"
+    } else {
+        awardsClass = "bigAwards"
+    }
+    function trimLength(words){
+        if(words.length < 30){
+            return words
+        } else {
+            words = words.substring(1,27) + "..."
+            return words
+        }
+    }
+
+    const awardDetails = thisPost.data.all_awardings.map(award => {
+        return (
+            <div>
+                <img src = {award.icon_url}  className="post--icon"/>
+                <span> {award.name} - {award.count}</span>
+                <p>{trimLength(award.description)}</p>
+                
+            </div>
+        )
+    })
+
     return (
         <div>
             <p className="post--back" onClick = {history.goBack}> <i className="ri-arrow-left-line"></i> Go Back</p>
@@ -19,10 +46,17 @@ export default function Post(){
                       
             <div className="post--card">
                 <img src = {thisPost.data.thumbnail} className = "post--images"/>
-                <p><a href = {`http://www.reddit.com${thisPost.data.permalink}`} target="_blank">  Link to the post  </a> </p>
+                {numAwardTypes > 0 && <h3>Award Details</h3>}
+                <div className={`post--${awardsClass}`}>
+                    {awardDetails}
+                </div>
+                
+                
             </div>
 
             <hr></hr>
+            
+            <p className="post--link"><a href = {`http://www.reddit.com${thisPost.data.permalink}` } target="_blank" >  Check Out the Original post by clicking here </a> </p>
             
             <div className = "post--metrics">
                 <p> 
